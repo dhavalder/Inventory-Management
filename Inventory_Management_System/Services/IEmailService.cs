46af7f46832c93725acdd53feca861ad48218246
Dhaval der <dhavalder40985@gmail.com>
2025-05-01 17:04:58 +0530

    resolve git file ignored

---
+++ Inventory_Management_System/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Inventory_Management_System.Models.Dto;
+
+namespace Inventory_Management_System.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailDto emailDto);
+    }
+}
